c160b1addfff298d99b57764ea05f813ec0a2173
BuddhikaSadun <sadunjayamaha917@gmail.com>
2025-09-20 20:05:50 +0530

    Updated app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import NavBar from "./components/layout/NavBar";
+import ContactMe from "./components/pages/ContactMe";
+import Projects from "./components/pages/Projects";
+import AboutMe from "./components/pages/AboutMe";
+import Introduction from "./components/pages/Introduction";
+
+import {
+  introSection,
+  aboutSection,
+  projectSection,
+  contactSection,
+} from "./assets/data";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavBar />
+      <div
+        className="absolute top-0 left-0 z-[-2] h-screen w-full bg-white 
+  bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_120%,rgba(0,163,255,0)_100%)]"
+      ></div>
+      <section id="Home">
+        <Introduction intro={introSection} />
+      </section>
+      <section id="about">
+        <AboutMe about={aboutSection} />
+      </section>
+      <section id="projects">
+        <Projects project={projectSection} />
+      </section>
+      <section id="contactUs">
+        <ContactMe contact={contactSection} />
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
